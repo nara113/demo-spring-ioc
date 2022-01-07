346d9ff2d401c55239fc38704fd663c00958fa06
sedovila <0554my@naver.com>
2022-01-08 06:48:30 +0900

    펑션을 사용한 빈 등록

--- src/main/java/springapplicationcontext/DemoApplication.java
+++ src/main/java/springapplicationcontext/DemoApplication.java
@@ -1,10 +1,15 @@
 package springapplicationcontext;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import out.OutService;
 import springapplicationcontext.configuration.ApplicationConfig;
 
 import java.util.Arrays;
@@ -13,7 +18,16 @@ import java.util.Arrays;
 public class DemoApplication {
 
     public  static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+//        SpringApplication.run(DemoApplication.class, args);
+
+        //빠르다. @Bean 으로 등록하는 것 보단 나
+        new SpringApplicationBuilder()
+                .sources(DemoApplication.class)
+                .initializers((ApplicationContextInitializer<GenericApplicationContext>) applicationContext -> {
+                    applicationContext.registerBean(OutService.class);
+                    applicationContext.registerBean(ApplicationRunner.class, () -> a -> System.out.println("runner!!"));
+                })
+                .run(args);
     }
 
     private static void method1(String[] args) {
