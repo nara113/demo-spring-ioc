7a117321874db6f655dd0e78255c45489f275b62
sedovila <0554my@naver.com>
2022-01-08 19:26:26 +0900

    ApplicationEventPublisher

--- src/main/java/springapplicationcontext/Event/MyEventHandler.java
+++ src/main/java/springapplicationcontext/Event/MyEventHandler.java
@@ -1,6 +1,6 @@
 package springapplicationcontext.Event;
 
-import org.springframework.context.event.EventListener;
+import org.springframework.context.event.*;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
@@ -15,5 +15,25 @@ public class MyEventHandler {
     public void handler(MyEvent myEvent) {
         System.out.println(Thread.currentThread());
         System.out.println("myEvent : " + myEvent.data);
+    }
+
+    @EventListener
+    public void handler(ContextRefreshedEvent event) {
+        System.out.println("Refreshed! " + Thread.currentThread());
+    }
+
+    @EventListener
+    public void handler(ContextStartedEvent event) {
+        System.out.println("Started! " + Thread.currentThread());
+    }
+
+    @EventListener
+    public void handler(ContextStoppedEvent event) {
+        System.out.println("Stopped! " + Thread.currentThread());
+    }
+
+    @EventListener
+    public void handler(ContextClosedEvent event) {
+        System.out.println("closed! " + Thread.currentThread());
     }
 }
