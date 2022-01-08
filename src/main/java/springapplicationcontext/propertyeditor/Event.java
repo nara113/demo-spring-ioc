39be9b30f19f7056ed92002e3fc028b0474b3176
sedovila <0554my@naver.com>
2022-01-08 23:12:27 +0900

    데이터 바인딩 추상화

---
+++ src/main/java/springapplicationcontext/propertyeditor/Event.java
@@ -0,0 +1,13 @@
+package springapplicationcontext.propertyeditor;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Event {
+
+    private Integer id;
+}
