8687e4b88ffb71ceba89531d4883b0e1ee9bd0cf
sedovila <0554my@naver.com>
2022-01-18 20:40:27 +0900

    hateoas

--- src/main/java/springapplicationcontext/profile/ProfileService.java
+++ src/main/java/springapplicationcontext/profile/ProfileService.java
@@ -3,7 +3,7 @@ package springapplicationcontext.profile;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-//@Service
+@Service
 //@Profile("test")
 public class ProfileService {
 }
